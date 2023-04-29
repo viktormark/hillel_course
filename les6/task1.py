e334937bf987a36972ca68099130132d66f87d11
viktormark <viktormarkovetskyi@gmail.com>
2023-04-29 20:33:01 +0300

    made task1

---
+++ les6/task1.py
@@ -0,0 +1,17 @@
+import math
+
+
+def check_power_of_two(n):
+    log2 = math.log2(n)
+    if log2 == int(log2):
+        print("Yes")
+    else:
+        print("No")
+
+
+check_power_of_two(8)
+
+
+# 1. Дано натуральне число N. Виведіть слово YES, якщо число N є точним ступенем двійки,
+# або слово NO інакше.
+# 8 - YES, 3 - NO
