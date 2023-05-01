5cf2f6d29606a355eaf29a0b1099770d2c57bc6b
viktormark <viktormarkovetskyi@gmail.com>
2023-05-01 18:21:43 +0300

    made main

---
+++ les6/main.py
@@ -0,0 +1,14 @@
+from les6.task1 import check_power_of_two
+from les6.task2 import square
+from les6.task3 import fermat
+from les6.task4 import change_list
+from les6.task5 import to_dict
+from les6.task6 import sum_range
+
+if __name__ == '__main__':
+    check_power_of_two(8)
+    print(square(5))
+    print(fermat(47))
+    print(change_list([1, 2, 3, 4, 5]))
+    print(to_dict([1, "str", 9, "name"]))
+    print(sum_range(3, 17))
