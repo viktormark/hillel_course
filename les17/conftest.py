a15091af035c10a4fe853ad7e18f46fcfedd0a7c
viktormark <viktormarkovetskyi@gmail.com>
2023-08-22 17:47:04 +0300

    update 22.08

--- les17/conftest.py
+++ les17/conftest.py
@@ -1,5 +1,8 @@
 
-
+import pytest
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 
 @pytest.fixture
 def driver():
